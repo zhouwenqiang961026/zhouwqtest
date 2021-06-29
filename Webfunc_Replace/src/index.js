2d4ab29b808a01b4e20e079285739932cba12b28
liangzc <1473239369@qq.com>
2021-06-29 13:50:49 +0800

    处理正则解析异常

--- Webfunc_Replace/src/index.js
+++ Webfunc_Replace/src/index.js
@@ -5,29 +5,30 @@
  *  参数1(字符串类型)，参数2(字符串类型)，参数3(字符串类型)
  *  返回值为字符串类型
  */
-vds.import("vds.object.*","vds.string.*");
+vds.import("vds.object.*","vds.string.*","vds.exception.*");
 var main = function(arg1,arg2,arg3,isRegexp) {
     if (vds.object.isUndefOrNull(arg1))
         arg1 = "";
 
     if (vds.object.isUndefOrNull(arg2))
-        throw new Error("被替换字符串为空，请检查");
+        throw vds.exception.newConfigException("被替换字符串为空，请检查");
     if (vds.object.isUndefOrNull(arg3))
-        throw new Error("替换字符串为空，请检查");
+        throw vds.exception.newConfigException("替换字符串为空，请检查");
 
     arg1 = String(arg1);
     arg2 = String(arg2);
     arg3 = String(arg3);
 
     if (isRegexp == true) {
-        try {
-            eval("var re = /" + arg2 + "/g;");
-        } catch (e) {
-            throw new Error("Replace函数的第2个参数需要正则表达式，请检查正则表达式是否合法：arg2=" + arg2 + ", " + e.message);
-        }
-        if(typeof(re) == "undefined"){
-            var re = null;
-        }
+        // try {
+        //     eval("var re = /" + arg2 + "/g;");
+        // } catch (e) {
+        //     throw vds.exception.newConfigException("Replace函数的第2个参数需要正则表达式，请检查正则表达式是否合法：arg2=" + arg2 + ", " + e.message);
+        // }
+        // if(typeof(re) == "undefined"){
+        //     var re = null;
+        // }
+        var re = new RegExp(arg2,"g");
         arg1 = vds.string.replace(arg1, re, arg3);
     } else
         // 替换所有匹配字符
