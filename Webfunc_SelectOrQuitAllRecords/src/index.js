0c264ab24df62e097cbb4cd0b33df37462bd9e80
cheung666 <cheung_bun@163.com>
2021-08-09 11:15:34 +0800

    前端函数改造-SelectOrQuitAllRecords

--- Webfunc_SelectOrQuitAllRecords/src/index.js
+++ Webfunc_SelectOrQuitAllRecords/src/index.js
@@ -1,30 +1,34 @@
 /**
  * 从指定的界面实体获取记录数 参数数量:1 参数1 表名 (字符串类型) 返回值为整数类型
  */
-vds.import("vds.ds.*","vds.exception.*");
-var main = function(dsName,operType) {
-    if (dsName==null)
+vds.import("vds.ds.*", "vds.exception.*", "vds.object.*");
+var main = function (dsName, operType) {
+    if (vds.object.isUndefOrNull(dsName))
         throw vds.exception.newConfigException("实体名称不允许为空，请检查");
-    if (operType==null)
+    if (vds.object.isUndefOrNull(operType))
         throw vds.exception.newConfigException("取消或选择类型没传参数，请检查");
+
     var datasource = vds.ds.lookup(dsName);
-    if(!datasource) 
+    if (!datasource)
         throw vds.exception.newConfigException("实体变量无法识别！");
+
     var allRecords = datasource.getAllRecords().toArray();
     var operRecords = [];
-    for(var i=0;i<allRecords.length;i++){
+    for (var i = 0; i < allRecords.length; i++) {
         var record = allRecords[i];
-        if(datasource.isSelectedRecord({record:record})){
+        if (datasource.isSelectedRecord(record)) {
             continue;
         }
         operRecords.push(record);
     }
-    if(operType=="select"){
-        datasource.selectRecords(operRecords,true);
-    }else{
-        datasource.selectRecords(allRecords,false);
+
+    datasource.markMultipleSelect();
+    if (operType == "select") {
+        datasource.selectRecords(operRecords);
+    } else {
+        datasource.unSelectRecords(allRecords);
     }
 
     return operType;
 }
-export{    main}
+export { main }
