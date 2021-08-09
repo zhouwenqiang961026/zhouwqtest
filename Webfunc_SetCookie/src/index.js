f3659132f6486c562f40e24a1d33dc0d68b1fbe5
cheung666 <cheung_bun@163.com>
2021-08-09 11:18:32 +0800

    前端函数改造 - SetCookie

--- Webfunc_SetCookie/src/index.js
+++ Webfunc_SetCookie/src/index.js
@@ -1,24 +1,22 @@
 /**
  * 设置cookie
  */
-vds.import("vds.object.*");
-var main = function(param) {
-    var args = param.getArgs();
+vds.import("vds.object.*", "vds.exception.*", "vds.cookie.*");
+var main = function (CookieName, CookieValue, Expires, Path, Domain, Secure, HttpOnly) {
 
-    var argsLen = args ? args.length : 0;
-
-    var CookieName = argsLen >= 1 ? args[0] : null; // 必需。cookie名称。
-    var CookieValue = argsLen >= 2 ? args[1] : null; // 必需。cookie值。
-    var Expires = argsLen >= 3 ? args[2] : "2500-01-01 00:00:00"; // 可选。cookie有效期。
-    var Path = argsLen >= 4 ? args[3] : "/"; // 可选。 cookie服务器路径
-    var Domain = argsLen >= 5 ? args[4] : null; // 可选。cookie域名。
-    var Secure = argsLen >= 6 ? args[5] : false; // 可选。是否通过安全的HTTPS 连接来传输cookie。值cookie是否仅通过安全的https,值为0或1，如果值为1，则cookie只能在https连接上有效，默认值为0，表示cookei在http和https连接上都有效。
-    var HttpOnly = argsLen >= 7 ? args[6] : true; // 可选。指定Cookie是否可以通过客户端脚本访问
+    CookieName = vds.object.isUndefOrNull(CookieName) ? null : CookieName; // 必需。cookie名称。
+    CookieValue = vds.object.isUndefOrNull(CookieValue) ? null : CookieValue; // 必需。cookie值。
+    Expires = vds.object.isUndefOrNull(Expires) ? "2500-01-01 00:00:00" : Expires; // 可选。cookie有效期。
+    Path = vds.object.isUndefOrNull(Path) ? "/" : Path; // 可选。 cookie服务器路径
+    Domain = vds.object.isUndefOrNull(Domain) ? null : Domain; // 可选。cookie域名。
+    Secure = vds.object.isUndefOrNull(Secure) ? false : Secure; // 可选。是否通过安全的HTTPS 连接来传输cookie。值cookie是否仅通过安全的https,值为0或1，如果值为1，则cookie只能在https连接上有效，默认值为0，表示cookei在http和https连接上都有效。
+    HttpOnly = vds.object.isUndefOrNull(HttpOnly) ? true : HttpOnly; // 可选。指定Cookie是否可以通过客户端脚本访问
 
     if (vds.object.isUndefOrNull(CookieName))
-        throw new Error("cookie名称为空，请检查");
+        throw vds.exception.newConfigException("cookie名称为空，请检查！");
     if (vds.object.isUndefOrNull(CookieValue))
-        throw new Error("cookie值为空，请检查");
+        throw vds.exception.newConfigException("cookie值为空，请检查");
+
     try {
         var options = {};
 
@@ -37,34 +35,30 @@ var main = function(param) {
             options.domain = Domain;
         }
         // 可选。是否通过安全的 HTTPS
-        if (typeof(Secure) == "string")
+        if (typeof (Secure) == "string")
             Secure = Secure.toLowerCase() == "true" ? true : false;
 
-        if (typeof(Secure) == "number")
+        if (typeof (Secure) == "number")
             Secure = Secure == 1;
 
         if (Secure != null) {
             options.secure = Secure;
         }
         // 可选。指定Cookie是否可以通过客户端脚本访问
-        if (typeof(HttpOnly) == "string")
+        if (typeof (HttpOnly) == "string")
             HttpOnly = HttpOnly.toLowerCase() == "true" ? true : false;
 
-        if (typeof(HttpOnly) == "number")
+        if (typeof (HttpOnly) == "number")
             HttpOnly = HttpOnly == 1;
 
         if (HttpOnly != null) {
             options.HttpOnly = HttpOnly;
         }
 
-        cookieUtil.vcookie({
-            "name": CookieName,
-            "value": CookieValue,
-            "options": options
-        });
+        vds.cookie.set(CookieName, CookieValue, options);
         return true;
     } catch (e) {
         return false;
     }
 }
-export{    main}
+export { main }
