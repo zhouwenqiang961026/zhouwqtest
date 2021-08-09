7e974d965b3af2f9a21b770419d030a91ce09cf6
cheung666 <cheung_bun@163.com>
2021-08-09 11:17:58 +0800

    前端函数改造 - SetControlProperty

--- Webfunc_SetControlProperty/src/index.js
+++ Webfunc_SetControlProperty/src/index.js
@@ -5,41 +5,41 @@ SetControlProperty("ControlCodeList","ControlPropertyCode","Value")
 参数2：字符串，必填，例如Visible、ReadOnly、Enable、Lable、Value==
 参数3：字符串，必填
  */
-vds.import("vds.exception.*");
-var main = function(controls, propertyName, propertyValue) {
-    if(controls == undefined || controls === "") {
+vds.import("vds.exception.*", "vds.log.*", "vds.widget.*", "vds.object.*");
+var main = function (controls, propertyName, propertyValue) {
+    if (vds.object.isUndefOrNull(controls)) {
         var exception = vds.exception.newConfigException("控件编码不能为空！");
         throw exception;
     }
     controls = controls.trim();
 
-    if(propertyName == undefined || propertyName === "") {
+    if (vds.object.isUndefOrNull(propertyName)) {
         var exception = vds.exception.newConfigException("属性名称不能为空！");
         throw exception;
     }
     propertyName = propertyName.trim();
 
-    if(propertyValue === undefined) {
-        var exception = vds.exception.newConfigException("属性值不能为undefined！");
+    if (vds.object.isUndefOrNull(propertyValue)) {
+        var exception = vds.exception.newConfigException("属性值不能为空！");
         throw exception;
     }
 
     var widget;
     var widgetCode = "";
     var ControlList = controls.split(",");
-    for(var i = 0; i < ControlList.length; i++) {
+    for (var i = 0; i < ControlList.length; i++) {
         widgetCode = ControlList[i].trim();
-        widget = widgetContext.get(widgetCode, "widgetObj");
-        if(widget) {
+        widget = vds.widget.getProperty(widgetCode, "widgetObj");
+        if (widget) {
             try {
-                widgetProperty.set(widgetCode, propertyName, propertyValue);
-            } catch(e) {
-                var widgetType = widgetContext.getType(widgetCode);
-                var chineseTitleName = widgetProperty.get(widgetCode, "SimpleChineseTitle");
+                vds.widget.execute(widgetCode, "set" + propertyName, [propertyValue]);
+            } catch (e) {
+                var widgetType = vds.widget.getType(widgetCode);
+                var chineseTitleName = vds.widget.execute(widgetCode, "SimpleChineseTitle");
                 vds.log.warn("[webfunc_SetControlProperty]:设置" + widgetCode + "(" + widgetType + ")属性" + propertyName + "值" + propertyValue + "出错。");
             }
         }
     }
 
 }
-export{    main}
+export { main }
