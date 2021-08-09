43e6b5655de61000e2a25b4bb262cb57ff15d3b9
cheung666 <cheung_bun@163.com>
2021-08-09 11:24:09 +0800

    前端函数改造 - SetGridColVisble

--- Webfunc_SetGridColVisble/src/index.js
+++ Webfunc_SetGridColVisble/src/index.js
@@ -7,42 +7,38 @@
     参数2：需要修改的实体字段名，必填（字段串类型，多个字段用英文逗号分隔）
     参数3：是否显示，必填（布尔类型）
  */
-vds.import("vds.exception.*");
-var main = function (param) {
+vds.import("vds.exception.*", "vds.object.*", "vds.widget.*");
+var main = function (widgetCode, fieldStr, visible) {
     //获取函数传入的参数
-    var args = param.getArgs();
-    var widgetCode = args[0];
-    var fieldStr = args[1];
-    var visible = args[2];
-    if( widgetCode== undefined || widgetCode=== ""){
+    if (vds.object.isUndefOrNull(widgetCode) || widgetCode === "") {
         var exception = vds.exception.newConfigException("列表控件名不能为空！");
-        throw exception;			
+        throw exception;
     }
-    if( fieldStr== undefined || fieldStr=== ""){
+    if (vds.object.isUndefOrNull(fieldStr) || fieldStr === "") {
         var exception = vds.exception.newConfigException("需要设置的字段名不能为空！");
-        throw exception;			
+        throw exception;
     }
-    if( visible== undefined || visible=== ""){
+    if (vds.object.isUndefOrNull(visible) || visible === "") {
         var exception = vds.exception.newConfigException("需要设置的显示值不能为空！");
-        throw exception;			
+        throw exception;
     }
-    var widget = widgetContext.get(widgetCode, "widgetObj");
+    var widget = vds.widget.getProperty(widgetCode, "widgetObj");
     var fieldList = fieldStr.split(",");
-    if(visible == "false" || visible == false){
+    if (visible == "false" || visible == false) {
         visible = false;
-    }else{
+    } else {
         visible = true;
     }
-    for(var i = 0;i< fieldList.length;i++){
+    for (var i = 0; i < fieldList.length; i++) {
         var field = widget.getFieldByName(fieldList[i]);
-        if(field){
-          if(visible){
+        if (field) {
+            if (visible) {
                 widget._widget.showField(field);
-            }else{
+            } else {
                 widget._widget.hideField(field);
             }
-        }  
+        }
     }
-    
+
 }
-export{    main}
+export { main }
